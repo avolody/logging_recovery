82b498186e43524354aa8197d7b394b8bd5018da
Jeremy <LJ@LJ-Studio>
2016-06-01 16:41:05 +0200

    Persistance Manager basic functions

---
+++ Aufgabe5/src/clients/PersistanceManager.java
@@ -0,0 +1,45 @@
+package clients;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author LJ
+ */
+public class PersistanceManager {
+
+// assumes the current class is called logger
+
+    /**
+     * starts a new transaction. The persistence manager creates a unique
+     * transaction ID and returns it to the client
+     *
+     * @return id
+     */
+    public int beginTransaction() {
+        return 0;
+    }
+
+    /**
+     * writes the given data with the given page ID on behalf of the given
+     * transaction to the buffer. If the given page already exists, its content
+     * is replaced completely by the given data
+     *
+     * @param taid
+     * @param pageid
+     * @param data
+     */
+    public void write(int taid, int pageid, String data) {
+    }
+
+    /**
+     * commits the transaction specified by the given transaction ID
+     *
+     * @param taid
+     */
+    public void commit(int taid) {
+        
+    }
+
+}
