51bbc22d4ea68f123d57ffb7ec269a580c69db3b
aminavoloder <aminavoloder@nat-inside-192-168-213-206.rrz.uni-hamburg.de.local>
2016-06-04 16:56:19 +0200

    lsn and log mngr

---
+++ Aufgabe5/src/managers/LogManager.java
@@ -0,0 +1,10 @@
+package managers;
+
+public class LogManager {
+
+	//Log createLog(int lsn, String className)
+	
+	//void writeLog(Writer writer, LogEntry logEntry) throws IOException 
+
+	//Log readLog(Reader reader) throws IOException
+}
--- Aufgabe5/src/managers/PersistenceManager.java
+++ Aufgabe5/src/managers/PersistenceManager.java
@@ -1,5 +1,9 @@
 package managers;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -79,5 +83,33 @@ public class PersistenceManager implements IPersistentManager {
         _buffer.clear();
         System.out.println("Buffer emptied");
     }
+    
+	void propagatePage(Page page){
+		try {
+			String content = page.getPageId() + "," + page.getLSN() + ","
+					+ page.getData() + "\n";
+
+			File file = new File(createPageFileName(page.getPageId()));
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+
+			System.out.println("Page is propagated (Page file is created)");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String createPageFileName(int x){
+		return "";
+	}
 
 }
